e7527655574591555a124c3c37af3f28dcec6ce9
manojvicky <manoj.save.earth@gmail.com>
2020-04-15 16:02:17 +0530

    bubble sort

---
+++ bubble sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(nums){
+    let sorted = nums.length;
+    let isSorted = true;
+    while(isSorted){
+        isSorted=false;
+        for(let j=0;j<sorted-1;j++){
+            if(nums[j]>nums[j+1]){
+                let temp = nums[j+1];
+                nums[j+1]=nums[j];
+                nums[j]=temp;
+                isSorted=true;
+            }
+        }
+        sorted--;
+    }
+    return nums;
+}
+
+console.log(bubbleSort([5,2,3,1]));
